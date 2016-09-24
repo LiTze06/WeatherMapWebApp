507a8a166e843f8c517e1ad63ee92d41fbc867d6
LiTze06 <hanlt016@hotmail.com>
2016-09-24 15:45:56 +1200

    removed google map

--- js/main.js
+++ js/main.js
@@ -1,153 +1,60 @@
-var map;
-var geoJSON;
 var request;
 var gettingData = false;
-var google;
-var value;
 var openWeatherMapKey = "6f6f42e65257899f09e1b2bc542f148b";
-function initMap() {
-    var mapOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(-36.8485, 174.7633)
-    };
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    // Add listeners to search city on google map
-    var geocoder = new google.maps.Geocoder();
-    document.getElementById("address").addEventListener('keyup', function (event) {
-        event.preventDefault();
-        geocodeAddress(geocoder, map);
-        if (event.keyCode == 13) {
-            document.getElementById("address").click();
-        }
-    });
-    document.getElementById('submitButton').addEventListener('click', function () {
-        geocodeAddress(geocoder, map);
-    });
-    // Add interaction listeners to make weather requests
-    google.maps.event.addListener(map, 'idle', checkIfDataRequested);
-    // Sets up and populates the info window with details
-    map.data.addListener('click', function (event) {
-        infowindow.setContent("<img src=" + event.feature.getProperty("icon") + ">"
-            + "<br /><strong>" + event.feature.getProperty("city") + "</strong>"
-            + "<br />" + event.feature.getProperty("temperature") + "&deg;C"
-            + "<br />" + event.feature.getProperty("weather"));
-        infowindow.setOptions({
-            position: {
-                lat: event.latLng.lat(),
-                lng: event.latLng.lng()
-            },
-            pixelOffset: {
-                width: 0,
-                height: -15
-            }
-        });
-        infowindow.open(map);
-    });
-}
-function geocodeAddress(geocoder, map) {
-    var address = document.getElementById('address').value;
-    geocoder.geocode({ 'address': address }, function (results, status) {
-        if (status === 'OK') {
-            map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location
-            });
-        }
-    });
-}
-var checkIfDataRequested = function () {
-    // Stop extra requests being sent
-    while (gettingData === true) {
-        request.abort();
-        gettingData = false;
+var submitButton = document.getElementById("submitButton");
+var cityInput = document.getElementById("cityInput");
+var displayResult = document.getElementById("displayResult");
+submitButton.addEventListener("click", function () {
+    ;
+    getWeather();
+});
+cityInput.addEventListener('keyup', function (event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        // 13 is ENTER
+        cityInput.click();
+        getWeather();
     }
-    getCoords();
-};
-// Get the coordinates from the Map bounds
-var getCoords = function () {
-    var bounds = map.getBounds();
-    var NE = bounds.getNorthEast();
-    var SW = bounds.getSouthWest();
-    getWeather(NE.lat(), NE.lng(), SW.lat(), SW.lng());
-};
-// Make the weather request
-var getWeather = function (northLat, eastLng, southLat, westLng) {
-    gettingData = true;
-    var requestString = "http://api.openweathermap.org/data/2.5/box/city?bbox="
-        + westLng + "," + northLat + "," //left top
-        + eastLng + "," + southLat + "," //right bottom
-        + map.getZoom()
-        + "&cluster=yes&format=json"
-        + "&APPID=" + openWeatherMapKey;
+});
+function getWeather() {
+    var city = cityInput.value;
+    var requestString = "http://api.openweathermap.org/data/2.5/weather?q="
+        + city + "&units=metric&format=json&APPID=" + openWeatherMapKey;
     request = new XMLHttpRequest();
-    request.onload = proccessResults;
-    request.open("get", requestString, true);
+    request.open("GET", requestString, true);
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200)
+            var temperatureInfo = JSON.parse(request.responseText);
+        processResults(temperatureInfo);
+    };
     request.send();
-};
-// Take the JSON results and proccess them
-var proccessResults = function () {
-    console.log(this);
-    var results = JSON.parse(this.responseText);
-    if (results.list.length > 0) {
-        resetData();
-        for (var i = 0; i < results.list.length; i++) {
-            geoJSON.features.push(jsonToGeoJson(results.list[i]));
-        }
-        drawIcons(geoJSON);
+}
+;
+var weatherInformation = (function () {
+    function weatherInformation(city, temperature, description, humidity, windSpeed, icon) {
+        this.city = city;
+        this.temperature = temperature;
+        this.description = description;
+        this.humidity = humidity;
+        this.windSpeed = windSpeed;
+        this.iconURL = "http://openweathermap.org/img/w/" + icon + ".png";
     }
+    return weatherInformation;
+}());
+var processResults = function (temperatureInfo) {
+    console.log(this);
+    var data = new weatherInformation(temperatureInfo.name, temperatureInfo.main.temp, temperatureInfo.weather[0].main, temperatureInfo.main.humidity, temperatureInfo.wind.speed, temperatureInfo.weather[0].icon);
+    var weathericon = document.getElementById("weatherIcon");
+    weathericon.innerHTML = "<img id='weatherImg' class='img-responsive' alt='Weather Icon' src='"
+        + data.iconURL + "'>";
+    var cityName = document.getElementById("city");
+    cityName.innerHTML = "<h3 id='cityName'>" + data.city + "</h3>";
+    var temperature = document.getElementById("temperature");
+    temperature.innerHTML = "<h3 id='temperatureInDegree'>" + data.temperature + "&#8451;</h3>";
+    var tableContent = "<table id='resultTable'>";
+    tableContent += "<tr><td>Description:</td><td>" + data.description + "</td><tr>";
+    tableContent += "<tr><td>Humidity:</td><td>" + data.humidity + "%</td><tr>";
+    tableContent += "<tr><td>Wind Speed:</td><td>" + data.windSpeed + "mps</td><tr>";
+    tableContent += "</table>";
+    displayResult.innerHTML = tableContent;
 };
-var infowindow = new google.maps.InfoWindow();
-// For each result that comes back, convert the data to geoJSON
-var jsonToGeoJson = function (weatherItem) {
-    var feature = {
-        type: "Feature",
-        properties: {
-            city: weatherItem.name,
-            weather: weatherItem.weather[0].main,
-            temperature: weatherItem.main.temp,
-            min: weatherItem.main.temp_min,
-            max: weatherItem.main.temp_max,
-            humidity: weatherItem.main.humidity,
-            pressure: weatherItem.main.pressure,
-            windSpeed: weatherItem.wind.speed,
-            windDegrees: weatherItem.wind.deg,
-            windGust: weatherItem.wind.gust,
-            icon: "http://openweathermap.org/img/w/"
-                + weatherItem.weather[0].icon + ".png",
-            coordinates: [weatherItem.coord.lon, weatherItem.coord.lat]
-        },
-        geometry: {
-            type: "Point",
-            coordinates: [weatherItem.coord.lon, weatherItem.coord.lat]
-        }
-    };
-    // Set the custom marker icon
-    map.data.setStyle(function (feature) {
-        return {
-            icon: {
-                url: feature.getProperty('icon'),
-                anchor: new google.maps.Point(25, 25)
-            }
-        };
-    });
-    // returns object
-    return feature;
-};
-// Add the markers to the map
-var drawIcons = function (weather) {
-    map.data.addGeoJson(geoJSON);
-    // Set the flag to finished
-    gettingData = false;
-};
-// Clear data layer and geoJSON
-var resetData = function () {
-    geoJSON = {
-        type: "FeatureCollection",
-        features: []
-    };
-    map.data.forEach(function (feature) {
-        map.data.remove(feature);
-    });
-};
-google.maps.event.addDomListener(window, 'load', initMap);
